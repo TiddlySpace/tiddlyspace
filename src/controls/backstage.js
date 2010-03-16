81845ae419d335f703e454317d2a4f7367c2597a
FND <fnd@oda.(none)>
2010-03-16 14:34:25 +0000

    added state-dependent user login to dummy plugin
    This is expected to be refactored into a generic plugin later on.

--- src/controls/backstage.js
+++ src/controls/backstage.js
@@ -5,4 +5,20 @@ config.tasks.tiddlyspace = {
 	content: "<<TiddlySpaceCP>>"
 };
 config.backstageTasks.push("tiddlyspace");
+
+config.macros.TiddlySpaceCP = {
+	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
+		if(config.options.txtUserName == "GUEST") {
+			console.log(place,
+			jQuery("<a>login</a>").
+				attr("href", "/challenge/openid?tiddlyweb_redirect=%2F").
+				appendTo(place));
+		} else {
+			jQuery("<dl />").
+				append("<dt>current user</dt>").
+				append("<dd>" + config.options.txtUserName + "</dd>").
+				appendTo(place);
+		}
+	}
+};
 //}}}
