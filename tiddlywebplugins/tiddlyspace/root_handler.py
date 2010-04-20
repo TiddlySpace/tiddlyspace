c457ea5c58d59e1c22c21dc020ca063dd6a2faef
Chris Dent <cdent@host46.msm.che.vodafone>
2010-04-20 20:21:48 +0800

    Extract _determine_host, fix some typos.
    The extraction of the _determine methods is in expectation of
    satisfying the Control Recipe and Bag Visibility Per Space
    story.

--- tiddlywebplugins/tiddlyspace/root_handler.py
+++ tiddlywebplugins/tiddlyspace/root_handler.py
@@ -10,14 +10,7 @@ def home(environ, start_response):
 
     relies on tiddlywebplugins.virtualhosting
     """
-    server_host = environ['tiddlyweb.config']['server_host']
-    port = int(server_host['port'])
-    if port == 80 or port == 443:
-        host_url = server_host['host']
-    else:
-        host_url = '%s:%s' % (server_host['host'], port)
-
-    http_host = environ.get('HTTP_HOST', host_url)
+    http_host, host_url = _determine_host(environ)
     if http_host == host_url:
         return serve_frontpage(environ, start_response)
     else:
@@ -41,14 +34,27 @@ def serve_space(environ, start_response, http_host):
     return get_tiddlers(environ, start_response)
 
 
+def _determine_host(environ):
+    server_host = environ['tiddlyweb.config']['server_host']
+    port = int(server_host['port'])
+    if port == 80 or port == 443:
+        host_url = server_host['host']
+    else:
+        host_url = '%s:%s' % (server_host['host'], port)
+
+    http_host = environ.get('HTTP_HOST', host_url)
+    return http_host, host_url
+
+
 def _determine_space_recipe(environ, space_name):
     store = environ['tiddlyweb.store']
+    user = environ['tiddlyweb.usersign']['name']
     bag = Bag("%s_private" % space_name)
     bag = store.get(bag)
     members = bag.policy.manage # XXX: authoritative?
 
     type = 'private' if user in members else 'public'
-    recipe_name = '%s_%s' % (space, type)
+    recipe_name = '%s_%s' % (space_name, type)
     return recipe_name
 
 
@@ -56,4 +62,6 @@ def _determine_space(environ, http_host):
     """
     calculates the space associated with a subdomain
     """
+    # XXX: This is broken for spaces which are not a subdomain
+    # of the main tiddlyspace domain.
     return http_host.split('.')[0]
