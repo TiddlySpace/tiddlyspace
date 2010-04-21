d17fe6d35a3e2402679713194997cbb8a2fc7286
FND <fnd@oda.(none)>
2010-04-21 12:04:33 +0100

    added dynamic hiding of backstage tasks based on user status
    If the current user is not logged in, the user and space tabs should not
    be visible. Otherwise, the login tab should be hidden.
    
    The current implementation is less than ideal because it requires the
    username to be known when backstage.show is invoked. However, that value
    is determined asynchronously (triggered by TiddlyWebConfig), so the
    backstage might be opened before the correct username is known.

--- src/controls/TiddlySpaceBackstage.js
+++ src/controls/TiddlySpaceBackstage.js
@@ -1,21 +1,23 @@
 //{{{
+(function($) {
+
 config.backstageTasks = [];
 
-config.tasks.login = { // TODO: dynamically hide if logged in
+config.tasks.login = {
 	text: "login",
 	tooltip: "TiddlySpace login",
 	content: "<<tiddler BackstageLogin>>"
 };
 config.backstageTasks.push("login");
 
-config.tasks.user = { // TODO: dynamically hide if not logged in
+config.tasks.user = {
 	text: "user",
 	tooltip: "user control panel",
 	content: "<<tiddler BackstageUser>>"
 };
 config.backstageTasks.push("user");
 
-config.tasks.space = { // TODO: dynamically hide if not logged in
+config.tasks.space = {
 	text: "space",
 	tooltip: "space control panel",
 	content: "<<tiddler BackstageSpace>>"
@@ -35,4 +37,17 @@ config.tasks.options = {
 	content: "<<tiddler BackstageOptions>>"
 };
 config.backstageTasks.push("options");
+
+var _show = backstage.show;
+backstage.show = function() { // XXX: not very safe, due to unknown time of evaluation
+	var tasks = $("#backstageToolbar .backstageTask").show();
+	if(config.options.txtUserName == "GUEST") {
+		tasks.slice(1, 3).hide();
+	} else {
+		tasks.eq(0).hide();
+	}
+	return _show.apply(this, arguments);
+};
+
+})(jQuery);
 //}}}
