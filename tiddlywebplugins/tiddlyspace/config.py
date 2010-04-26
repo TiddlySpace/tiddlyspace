6abb75e96a8164eead0b3a1bcaa2ee5eeb4704a3
FND <fnd@oda.(none)>
2010-04-26 10:28:03 +0100

    limited bag/recipe creation to authenticated users
    anonymous users have no active role in the TiddlySpace concept

--- tiddlywebplugins/tiddlyspace/config.py
+++ tiddlywebplugins/tiddlyspace/config.py
@@ -8,5 +8,7 @@ PACKAGE_NAME = 'tiddlywebplugins.tiddlyspace'
 
 config = {
     'instance_tiddlers': get_tiddler_locations(store_contents, PACKAGE_NAME),
-    'auth_systems': ['%s.challenger' % PACKAGE_NAME]
+    'auth_systems': ['%s.challenger' % PACKAGE_NAME],
+    'bag_create_policy': 'ANY',
+    'recipe_create_policy': 'ANY'
 }
