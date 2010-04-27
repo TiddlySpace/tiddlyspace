d23472d86f5d9fb49c887469b0d79fd8901ffea9
Chris Dent <cdent@host46.msm.che.vodafone>
2010-04-27 12:10:24 +0100

    Remove the tiddlyspace challenger in favor of a cookie modifying middleware (to come).

--- tiddlywebplugins/tiddlyspace/config.py
+++ tiddlywebplugins/tiddlyspace/config.py
@@ -8,7 +8,7 @@ PACKAGE_NAME = 'tiddlywebplugins.tiddlyspace'
 
 config = {
     'instance_tiddlers': get_tiddler_locations(store_contents, PACKAGE_NAME),
-    'auth_systems': ['%s.challenger' % PACKAGE_NAME, 'tiddlywebplugins.openid2'],
+    'auth_systems': ['cookie_form', 'tiddlywebplugins.openid2'],
     'bag_create_policy': 'ANY',
     'recipe_create_policy': 'ANY',
     'socialusers.reserved_names': ['www', 'about', 'help', 'announcements', 'info'],
