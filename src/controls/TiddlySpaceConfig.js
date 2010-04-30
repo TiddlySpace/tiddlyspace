4f56e78371cf0bb671d482e8409e706ad97eb82f
FND <fnd@oda.(none)>
2010-04-30 11:12:42 +0100

    added TiddlySpaceConfig plugin providing info about current space

---
+++ src/controls/TiddlySpaceConfig.js
@@ -0,0 +1,10 @@
+/***
+|''Requires''|TiddlyWebConfig|
+***/
+//{{{
+config.extensions.TiddlyWeb.tiddlyspace = {
+	currentSpace: config.defaultCustomFields["server.workspace"].
+		split("recipes/")[1]. // XXX: brittle?
+		split("_")[0] // XXX: brittle (space name must not contain underscores)
+};
+//}}}
--- src/controls/TiddlySpaceSidebar.js
+++ src/controls/TiddlySpaceSidebar.js
@@ -1,5 +1,5 @@
 /***
-|''Requires''|TiddlyWebConfig|
+|''Requires''|TiddlySpaceConfig|
 ***/
 //{{{
 (function($) {
@@ -13,12 +13,9 @@ var shadows = config.shadowTiddlers;
 shadows.SideBarOptions = shadows.SideBarOptions.replace("<<saveChanges>>",
 	"<<tiddler TiddlySpaceControls>><<saveChanges>>");
 
-var space = config.defaultCustomFields["server.workspace"].
-	split("recipes/")[1]. // XXX: brittle?
-	split("_")[0]; // XXX: brittle (space name must not contain underscores)
-
 shadows.TiddlySpaceControls = $.map(["public", "private"], function(item, i) {
 	var label = plugin.btnLabel.format([item]); // XXX: i18n?!
+	var space = config.extensions.TiddlyWeb.tiddlyspace.currentSpace;
 	return plugin.btnTemplate.format([space, item, label]);
 }).join("");
 
