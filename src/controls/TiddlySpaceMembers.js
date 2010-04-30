e61b8aff1c774bd0ed504b54fba979bf5c923ee5
FND <fnd@oda.(none)>
2010-04-30 11:17:02 +0100

    rewrote members macro to use the new Space API

--- src/controls/TiddlySpaceMembers.js
+++ src/controls/TiddlySpaceMembers.js
@@ -1,46 +1,72 @@
 /***
-|''Requires''|TiddlyWebConfig|
+|''Requires''|TiddlySpaceConfig|
+!HTMLForm
+<form action="#">
+	<fieldset>
+		<legend />
+		<dl>
+			<dt>Username:</dt>
+			<dd><input type="text" name="username" /></dd>
+		</dl>
+		<input type="submit" />
+	</fieldset>
+</form>
+!Code
 ***/
 //{{{
 (function($) {
 
+var cfg = config.extensions.TiddlyWeb;
+
 var macro = config.macros.TiddlySpaceMembers = {
-	label: "Add member",
-	msgSuccess: "added member %0",
-	msgError: "error adding member %0: %1",
+	formTemplate: store.getTiddlerText(tiddler.title + "##HTMLForm"),
+	locale: {
+		listError: "error retrieving members for space %0: %1",
+		addLabel: "Add member",
+		addSuccess: "added member %0",
+		addError: "error adding member %0: %1"
+	},
 
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
-		var container = $("<fieldset />");
-		$("<legend />").text(this.label).appendTo(container);
-		$(container).
-			append('<input type="text" value="username" />');
-		$('<input type="submit" />').val(this.label).
-			click(this.onClick).
-			appendTo(container);
-		$('<form action="" />').append(container).appendTo(place);
+		macro.space = new tiddlyweb.Space(cfg.tiddlyspace.currentSpace, cfg.host);
+		var container = $("<div />").appendTo(place);
+		this.refresh(container);
 	},
-	onClick: function(ev) {
-		var btn = $(this);
-		var spaceName = config.defaultCustomFields["server.workspace"]. // XXX: DRY; cf. TiddlySpaceSidebar
-			split("recipes/")[1]. // XXX: brittle?
-			split("_")[0]; // XXX: brittle (space name must not contain underscores)
-		var username = btn.siblings("input[type=text]").val();
-		var host = config.defaultCustomFields["server.host"];
-
-		var getCallback = function(space, status, xhr) {
-			space.members.pushUnique(username);
-			space = new tiddlyweb.Space(space.name, space.members, space.host); // XXX: required because policies are only calculated on instantiation
-			space.put(putCallback, errback);
-		};
-		var putCallback = function(resource, status, xhr) {
-			displayMessage(macro.msgSuccess.format([username]));
+	refresh: function(container) {
+		var callback = function(data, status, xhr) {
+			container.empty();
+			macro.displayMembers(data, container);
 		};
 		var errback = function(xhr, error, exc) {
-			displayMessage(macro.msgError.format([username, xhr.statusText]));
+			displayMessage(macro.locale.listError.format([macro.space.name, error]));
 		};
-
-		var space = new tiddlyweb.Space(spaceName, [null], host);
-		space.get(getCallback, errback);
+		macro.space.members().get(callback, errback);
+	},
+	displayMembers: function(members, container) {
+		var items = $.map(members, function(item, i) {
+			return $("<li />").text(item)[0];
+		});
+		$("<ul />").append(items).appendTo(container);
+		macro.generateForm().appendTo(container);
+	},
+	generateForm: function() {
+		return $(macro.formTemplate).
+			find("legend").text(macro.locale.addLabel).end().
+			find("input[type=submit]").val(macro.locale.addLabel).
+				click(macro.onSubmit).end();
+	},
+	onSubmit: function(ev) {
+		var form = $(this).closest("form");
+		var username = form.find("input[name=username]").val();
+		var callback = function(resource, status, xhr) {
+			displayMessage(macro.locale.addSuccess.format([username]));
+			var container = form.closest("div");
+			macro.refresh(container);
+		};
+		var errback = function(xhr, error, exc) {
+			displayMessage(macro.locale.addError.format([username, error]));
+		};
+		macro.space.members().add(username, callback, errback);
 		return false;
 	}
 };
