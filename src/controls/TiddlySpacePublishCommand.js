6180180c87778306caa4310c73987155b0815118
FND <fnd@oda.(none)>
2010-05-07 12:48:19 +0100

    added error handling to publishTiddlerRevision command
    also fixed obsolete this.determineSpace reference

--- src/controls/TiddlySpacePublishCommand.js
+++ src/controls/TiddlySpacePublishCommand.js
@@ -2,28 +2,39 @@
 |''Requires''|TiddlySpaceConfig|
 ***/
 //{{{
-config.commands.publishTiddlerRevision = {
+(function() {
+
+var determineSpace = config.extensions.tiddlyspace.determineSpace;
+
+var cmd = config.commands.publishTiddlerRevision = {
 	text: "publish",
 	tooltip: "Make this revision public",
+	errorMsg: "Error publishing %0: %1",
 
 	isEnabled: function(tiddler) {
 		var title = tiddler.title;
 		if(store.isShadowTiddler(title) && !store.tiddlerExists(title)) {
 			return false;
 		}
-		var space = config.extensions.tiddlyspace.determineSpace(tiddler);
+		var space = determineSpace(tiddler);
 		return space && space.type == "private";
 	},
 	handler: function(ev, src, title) {
 		var tiddler = store.getTiddler(title);
-		var space = this.determineSpace(tiddler);
+		var space = determineSpace(tiddler);
 		// XXX: changes to tiddler object need to be reverted on error!?
-		tiddler.fields["server.page.revision"] = "false";
 		tiddler.fields["server.workspace"] = "bags/%0_public".format([space.name]);
+		tiddler.fields["server.page.revision"] = "false";
 		var adaptor = tiddler.getAdaptor();
 		adaptor.putTiddler(tiddler, null, null, function(context, userParams) {
-			story.refreshTiddler(context.tiddler.title, null, true);
+			if(context.status) {
+				story.refreshTiddler(context.tiddler.title, null, true);
+			} else {
+				displayMessage(cmd.errorMsg.format([title, context.statusText]));
+			}
 		});
 	}
 };
+
+})();
 //}}}
