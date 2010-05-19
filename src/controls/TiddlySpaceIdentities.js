66a86b9f7629f655f6596dd7ccf2da29121a7364
FND <fnd@oda.(none)>
2010-05-19 13:44:26 +0100

    added support for listing identities
    code structure (use of refresh method) chosen for consistency with
    TiddlySpaceMembers, TiddlySpaceSpaces and TiddlySpaceUserControls

--- src/controls/TiddlySpaceIdentities.js
+++ src/controls/TiddlySpaceIdentities.js
@@ -20,7 +20,6 @@
 	</fieldset>
 </form>
 !TODO
-* i18n (form labels)
 * process config.extensions.tiddlyweb.challengers instead of hardcoding OpenID
 !Code
 ***/
@@ -32,22 +31,42 @@ var ns = config.extensions.tiddlyweb;
 var macro = config.macros.TiddlySpaceIdentities = {
 	formTemplate: store.getTiddlerText(tiddler.title + "##HTMLForm"),
 	locale: {
-		label: "Add Identity"
+		listError: "error retrieving identities for user %0",
+		addLabel: "Add Identity"
 	},
 
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
 		ns.getUserInfo(function(user) {
 			if(!user.anon) {
-				var challenger = "tiddlywebplugins.tiddlyspace.openid";
-				var uri = "%0/challenge/%1".format([ns.host, challenger]);
-				var redirect = ns.serverPrefix + "#auth:OpenID";
-				$(macro.formTemplate).attr("action", uri).
-					find("legend").text(macro.locale.label).end().
-					find("[name=tiddlyweb_redirect]").val(redirect).end().
-					find("[type=submit]").val(macro.locale.label).end().
-					appendTo(place);
+				var container = $("<div />").appendTo(place);
+				macro.refresh(container);
 			}
 		});
+	},
+	refresh: function(container) {
+		container.empty().append("<ul />").append(this.generateForm());
+		$.ajax({  // TODO: add (dynamically) to chrjs user extension?
+			url: "%0/users/%1/identities".format([ns.host, ns.username]),
+			type: "GET",
+			success: function(data, status, xhr) {
+				var identities = $.map(data, function(item, i) {
+					return $("<li />").text(item)[0];
+				});
+				$("ul", container).append(identities);
+			},
+			error: function(xhr, error, exc) {
+				displayMessage(macro.locale.listError.format([ns.username]));
+			}
+		});
+	},
+	generateForm: function() {
+		var challenger = "tiddlywebplugins.tiddlyspace.openid";
+		var uri = "%0/challenge/%1".format([ns.host, challenger]);
+		var redirect = ns.serverPrefix + "#auth:OpenID";
+		return $(this.formTemplate).attr("action", uri).
+			find("legend").text(this.locale.addLabel).end().
+			find("[name=tiddlyweb_redirect]").val(redirect).end().
+			find("[type=submit]").val(this.locale.addLabel).end();
 	}
 };
 
