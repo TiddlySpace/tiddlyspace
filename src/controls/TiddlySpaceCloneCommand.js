cf77acb7a7f79e78caa22766818d29c9c502b5d3
FND <fnd@oda.(none)>
2010-05-27 13:55:22 +0100

    replaced editTiddler command with cloneTiddler for external tiddlers
    For now, cloneTiddler's button label (though not tooltip) imitates
    editTiddler's to make it appear as the same operation to users. This
    might be reverted later on.
    
    Note that this imitiation of editTiddler does not extend to special
    attributes, namely default toolbar action ("+"), requiring users to more
    explicitly select this operation than when editing existing tiddlers.

--- src/controls/TiddlySpaceCloneCommand.js
+++ src/controls/TiddlySpaceCloneCommand.js
@@ -4,10 +4,11 @@
 //{{{
 (function($) {
 
+var cmd = config.commands;
 var ns = config.extensions.tiddlyspace;
 
-var cmd = config.commands.cloneTiddler = {
-	text: "clone",
+cmd.cloneTiddler = {
+	text: cmd.editTiddler.text,
 	tooltip: "Create a copy of this tiddler in the current space",
 	errorMsg: "Error publishing %0: %1",
 
@@ -21,9 +22,13 @@ var cmd = config.commands.cloneTiddler = {
 		$.each(["bag", "permissions", "page.revision"], function(i, item) {
 			delete tiddler.fields["server." + item];
 		});
-		config.commands.editTiddler.handler.apply(this, arguments);
+		cmd.editTiddler.handler.apply(this, arguments);
 		return false;
 	}
+};
+
+cmd.editTiddler.isEnabled = function(tiddler) {
+	return !cmd.cloneTiddler.isEnabled.apply(this, arguments);
 };
 
 })(jQuery);
