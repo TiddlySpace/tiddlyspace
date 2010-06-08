686865fa0ccbc1acb82f712afc906fa2cd709cda
bengillies <bengillies@gmail.com>
2010-06-08 16:27:50 +0100

    Added some custom scrolling behaviour
    Clicking a link now scrolls the footer up near the top of the page.
    This is done so that the content of the tiddler opened up is better exposed
    as it scrolls the bubbles off the screen.
    
    This has only limited use at the moment, as it is expected that in the future,
    extra content/tiddlers will be added to the frontpage (such as about, help,
    or FAQ tiddlers), which will make the scrolling behavious more important.

---
+++ src/frontpage/FrontPageConfig.js
@@ -0,0 +1,30 @@
+/***
+|TableViewPlugin|
+|Version|0.1|
+|Author|Ben Gillies|
+***/
+//{{{
+(function($) {
+//ensure that the screen scrolls to a desired height.
+//in this case, we want it to scroll almost (but not quite) 1 entire
+//screen
+_Scroller = Scroller;
+Scroller = function(targetElement) {
+	var offset = 50;
+	var endPoint = ensureVisible(targetElement);
+	var minScroll = $('#footer').offset().top - offset;
+	if (endPoint < minScroll) {
+		endPoint = minScroll;
+	}
+	//make sure the page has enough space to scroll into
+	var tiddlerDisplay = $('#tiddlerDisplay');
+	if ($(tiddlerDisplay).height() < (screen.height - offset)) {
+		$(tiddlerDisplay).height(screen.height - offset);
+	}
+
+	var p=[{style: "-tw-vertScroll", start: findScrollY(), end: endPoint}];
+
+	return new Morpher(targetElement, config.animDuration, p);
+}
+})(jQuery);
+//}}}
