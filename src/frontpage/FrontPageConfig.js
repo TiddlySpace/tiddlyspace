628ada65f8e381c8e8544825df311a98d1469b35
FND <fnd@oda.(none)>
2010-06-09 10:46:40 +0100

    improved comments, stylistic consistency in FrontPageConfig
    cf. doc/STYLE

--- src/frontpage/FrontPageConfig.js
+++ src/frontpage/FrontPageConfig.js
@@ -5,26 +5,27 @@
 ***/
 //{{{
 (function($) {
-//ensure that the screen scrolls to a desired height.
-//in this case, we want it to scroll almost (but not quite) 1 entire
-//screen
-_Scroller = Scroller;
+
+// override Scroller to ensure that the screen scrolls to a desired height
+// in this case, we want it to scroll almost (but not quite) 1 entire screen
+_Scroller = Scroller; // keep original, just in case...
 Scroller = function(targetElement) {
 	var offset = 50;
 	var endPoint = ensureVisible(targetElement);
-	var minScroll = $('#footer').offset().top - offset;
+	var minScroll = $("#footer").offset().top - offset;
 	if (endPoint < minScroll) {
 		endPoint = minScroll;
 	}
-	//make sure the page has enough space to scroll into
-	var tiddlerDisplay = $('#tiddlerDisplay');
+	// make sure the page has enough space to scroll into
+	var tiddlerDisplay = $("#tiddlerDisplay");
 	if ($(tiddlerDisplay).height() < (screen.height - offset)) {
 		$(tiddlerDisplay).height(screen.height - offset);
 	}
 
-	var p=[{style: "-tw-vertScroll", start: findScrollY(), end: endPoint}];
+	var p = [{ style: "-tw-vertScroll", start: findScrollY(), end: endPoint }];
 
 	return new Morpher(targetElement, config.animDuration, p);
-}
+};
+
 })(jQuery);
 //}}}
