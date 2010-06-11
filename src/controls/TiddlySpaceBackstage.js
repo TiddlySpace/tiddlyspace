9d870aa1c9e8a8d0e016260bed8d7b5ef0029fcd
Jon Robson <jdlrobson@gmail.com>
2010-06-11 13:42:07 +0100

    Overhaul of Backstage
    simplified backstage to have two buttons - space and user. All other commands will then go under here.
    Added extra behaviour to backstage:
      now on by default
      scrolling in the TiddlyWiki auto hides it
      scrolling to top of TiddlyWiki displays it after given time
      hovering over button reveals it
      SiteIcon (avatar) of space and user are shown next to the two menu options

--- src/controls/TiddlySpaceBackstage.js
+++ src/controls/TiddlySpaceBackstage.js
@@ -1,14 +1,11 @@
+/***
+|''Requires''|TiddlySpaceConfig|
+***/
 //{{{
 (function($) {
 
+config.options.chkBackstage = true;
 config.backstageTasks = [];
-
-config.tasks.autoSave = {
-	text: "autosave",
-	tooltip: "toggle automatic saving",
-	action: function() {}
-};
-config.backstageTasks.push("autoSave");
 
 config.tasks.login = {
 	text: "login",
@@ -18,47 +15,21 @@ config.tasks.login = {
 config.backstageTasks.push("login");
 
 config.tasks.user = {
-	text: "user",
+	text: "user: "+config.options.txtUserName,
 	tooltip: "user control panel",
 	content: "<<tiddler BackstageUser>>"
 };
 config.backstageTasks.push("user");
 
 config.tasks.space = {
-	text: "space",
+	text: "space: "+config.extensions.tiddlyspace.currentSpace.name,
 	tooltip: "space control panel",
-	content: "<<tiddler BackstageSpace>>"
+	content: "<<tiddler BackstageSpace>>",
+	className: "right"
 };
 config.backstageTasks.push("space");
 
-config.tasks.tiddlers = {
-	text: "tiddlers",
-	tooltip: "tiddlers control panel",
-	content: "<<tiddler BackstageTiddlers>>"
-};
-config.backstageTasks.push("tiddlers");
-
-config.tasks.options = {
-	text: "options",
-	tooltip: "TiddlyWiki options",
-	content: "<<tiddler BackstageOptions>>"
-};
-config.backstageTasks.push("options");
-
-config.tasks.tweak = {
-	text: "tweak",
-	tooltip: "Tweak the appearance and behaviour of TiddlyWiki",
-	content: "<<options>>"
-};
-config.backstageTasks.push("tweak");
-
-config.tasks.plugins = {
-	text: "plugins",
-	tooltip: "Manage installed plugins",
-	content: "<<plugins>>"
-};
-config.backstageTasks.push("plugins");
-
+config.messages.backstage.prompt = "";
 // initialize state
 var _show = backstage.show;
 backstage.show = function() {
@@ -71,14 +42,94 @@ backstage.show = function() {
 			tasks.eq(0).hide();
 		}
 	});
-	// add AutoSave option
-	var place = $('<span task="autoSave" />').
-		replaceAll("#backstageToolbar [task=autoSave]")[0];
-	invokeMacro(place, "option", "chkAutoSave");
-	$("<span>autosave</span>").appendTo(place); // TODO: i18n
 	// display backstage
 	return _show.apply(this, arguments);
 };
+
+var _init = backstage.init;
+backstage.init = function(){
+	var result = _init.apply(this, arguments);
+	
+	//make the backstage become visible when you mouseover it
+	var _revealBackstageArea;
+	$("#backstageButton").mouseover(function(e){ //when mouseover the button set a timeout to show backstage
+		if(!backstage.isVisible()){
+			_revealBackstageArea = window.setTimeout(function(){
+				if(!backstage.isVisible())backstage.show();
+			},"600");}
+		}
+	).
+	mouseout(function(e){ //on a mouseout we prevent showing of the backstage.
+		if(_revealBackstageArea){
+			window.clearTimeout(_revealBackstageArea);
+		}
+	});
+	
+	//override show button with an svg image
+	var showButton = $("#backstageShow")[0];
+	$(showButton).html("");
+	wikify("<<image backstage.svg 60 60>>",showButton);
+	
+	//override hide button
+	var hideButton =$("#backstageHide")[0];
+	jQuery(hideButton).html("");
+	wikify("<<image close.svg 25 25>>",hideButton);
+	
+	var backstageToolbar = $("#backstageToolbar")[0];
+	$("<div id='backstageLogo'></div>").prependTo(backstageToolbar);
+	wikify("<<image tiddlyspace.svg 16 16>> ''{{privateLightText{tiddly}}} {{publicLightText{space}}}''",$("#backstageLogo",backstageToolbar)[0]);
+	
+	var siteIcon =store.getTiddler("SiteIcon") 
+	if(siteIcon){
+		wikify(siteIcon.text,jQuery("[task=space]","#backstageArea")[0]);
+	}
+	
+	var host = config.defaultCustomFields['server.host'];
+	var subdomainStart = host.indexOf(".");
+	var addressStart = host.indexOf("/") + 2;
+	var tsHost = host.substr(0,addressStart)+ host.substr(subdomainStart+1);
+	
+	//show avatar in the users public bag
+	jQuery("[task=user]","#backstageArea").append('<span><img src="'+tsHost+'/recipes/'+config.options.txtUserName+'_public/tiddlers/SiteIcon"/></span><br/>');
+	//show default avatar for the login button
+	jQuery("[task=login]","#backstageArea").append('<span><img src="'+tsHost+'/recipes/tiddlyspace/tiddlers/tiddlers/SiteIcon"/></span><br/>');
+	
+}
+
+var _showPanel = backstage.showPanel;
+backstage.showPanel = function(){
+	jQuery("#backstageArea,#backstage,#backstageButton").css({position:"absolute"});	
+	_showPanel();	
+}
+
+var _hidePanel = backstage.hidePanel;
+backstage.hidePanel = function(e){
+	jQuery("#backstageArea,#backstage,#backstageButton").css({position:"fixed"});	
+	_hidePanel();
+}
+
+var timeout,mouseover;
+//keep a record of when the user has their mouse over the backstage to prevent annoying them
+jQuery("#backstageArea").mouseover(function(e){mouseover = true;})
+.mouseout(function(e){mouseover=false;});
+
+//setup a scroll event to give backstage certain behaviours
+jQuery(window).scroll(function(e){
+	if(window.scrollY > 0){ //if scrolling away from the top prepare to hide backstage after given time
+		window.clearTimeout(timeout);
+		timeout = window.setTimeout(function(){
+			if(!backstage.isPanelVisible() && !mouseover)backstage.hide();
+			actionInProgress = false;
+		},1000);
+	}
+	else if(window.scrollY === 0){ //if at top of screen show the backstage after given time
+		window.clearTimeout(timeout);
+		timeout = window.setTimeout(function(){
+			if(!backstage.isVisible())backstage.show();
+			actionInProgress = false;
+		},400);
+	}
+});
 
 })(jQuery);
 //}}}
