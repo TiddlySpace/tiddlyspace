dad40f45778956ee06da5762c699f4bd02829a98
Chris Dent <cdent@oxide.local>
2010-06-18 17:00:36 +0100

    Make handling of cookies when adding identities more robust.
    The prior code was liable to fail when presented with a cookie
    representing and OpenID with a ':' in it.
    
    It was also not strict about how it got cookies and their values.
    
    This solution is far more cumbersome but works. Borrowed from
    quirksmode.

--- src/plugins/TiddlySpaceIdentities.js
+++ src/plugins/TiddlySpaceIdentities.js
@@ -76,12 +76,23 @@ config.paramifiers.auth = {
 		error: "error adding identity %0: %1"
 	},
 
+	readCookie: function(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(";");
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==" ") c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	},
+
 	onstart: function(v) {
-		var identity = document.cookie.split("tiddlyweb_secondary_user=")[1];
+		var identity = this.readCookie("tiddlyweb_secondary_user");
 		if(identity) {
-			var name = identity.split(':')[0]; // XXX: brittle; name must not contain colon
-			name = name.replace('"', ''); // XXX: hacky!?
-			this.addIdentity(name);
+			// strip off the MAC from the cookie and unquote
+			identity = identity.replace(/:[^:]+$/, '').replace('"', '');
+			this.addIdentity(identity);
 		}
 	},
 	addIdentity: function(name) {
