562fb6fa2b57aa12765bf749d4753cec5c1d3240
Chris Dent <chris.dent@gmail.com>
2010-06-24 13:56:55 +0100

    Require openid2 plugin version 0.5
    This version uses tiddlyweb's logging facility, not python-openid's
    writing to stderr.

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'tiddlywebplugins.virtualhosting',
         'tiddlywebplugins.socialusers>=0.3',
         'tiddlywebplugins.magicuser>=0.2',
-        'tiddlywebplugins.openid2>=0.4',
+        'tiddlywebplugins.openid2>=0.5',
         'tiddlywebplugins.cookiedomain>=0.2',
         'tiddlywebplugins.mselect',
         'tiddlywebplugins.prettyerror',
