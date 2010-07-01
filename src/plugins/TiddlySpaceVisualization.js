7e711488d53081107e307aa9bf6a92ff26fa5f67
FND <fnd@oda.(none)>
2010-07-01 16:33:56 +0100

    added icon identifying a story tiddler as public/private
    This is just a first stab; style tweaks (size, spacing) and
    optimizations to follow - along with support for avatars on external
    tiddlers.
    
    NB: The icon is displayed along with the title (view macro) - so if that
    is removed or hidden, so will the icon. This is currently intentional,
    based on the assumption that title and icon are a unit.

--- src/plugins/TiddlySpaceVisualization.js
+++ src/plugins/TiddlySpaceVisualization.js
@@ -1,5 +1,5 @@
 /***
-|''Requires''|TiddlySpaceConfig|
+|''Requires''|TiddlySpaceConfig ImageMacroPlugin|
 !StyleSheet
 .public,
 .private,
@@ -19,10 +19,14 @@
 //{{{
 (function($) {
 
+if(!config.macros.image) {
+	throw "Missing dependency: ImageMacroPlugin";
+}
+
 // hijack refreshTiddler to add private/public/external class to story tiddlers
 var _refreshTiddler = Story.prototype.refreshTiddler;
 Story.prototype.refreshTiddler = function(title, template, force, customFields,
-	defaultText) {
+		defaultText) { // TODO: move into text viewer hijack below?
 	var el = _refreshTiddler.apply(this, arguments);
 	var tiddler = store.getTiddler(title);
 	if(tiddler) {
@@ -32,6 +36,22 @@ Story.prototype.refreshTiddler = function(title, template, force, customFields,
 		$(el).removeClass("private public external").addClass(type);
 	}
 	return el;
+};
+
+// hijack text viewer to add public/private icon
+var _view = config.macros.view.views.text;
+config.macros.view.views.text = function(value, place, params, wikifier,
+		paramString, tiddler) {
+	if(params[0] == "title") {
+		var ns = config.extensions.tiddlyspace;
+		var space = ns.determineSpace(tiddler, true);
+		var type = space && space.name == ns.currentSpace.name ? space.type : "external";
+		if(type != "external") {
+			invokeMacro(place, "image", "%0Icon alt:%0".format([type]), null, tiddler); // XXX: should call macro's function directly
+		}
+		$(place).attr("title", "private");
+	}
+	_view.apply(this, arguments);
 };
 
 var name = "StyleSheetTiddlySpace";
