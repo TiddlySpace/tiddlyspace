1c62653e7441642019dc9c3320ceac4e1481395f
FND <fnd@oda.(none)>
2010-07-07 10:12:23 +0100

    added support for publishing binary tiddlers
    An extra GET ensures we're dealing with the authentic (base64-encoded)
    original data, as the TiddlyWiki serialization does not guarantee this.
    
    NB: This requires TiddlyWebAdaptor 1.3.0:
    http://trac.tiddlywiki.org/changeset/12256

--- src/plugins/TiddlySpacePublishCommand.js
+++ src/plugins/TiddlySpacePublishCommand.js
@@ -31,13 +31,28 @@ var cmd = config.commands.publishTiddlerRevision = {
 	publishTiddler: function(title, callback) {
 		var original = store.getTiddler(title);
 		var space = ns.determineSpace(original);
-		var tiddler = $.extend(new Tiddler(title), original);
-		tiddler.fields = $.extend({}, original.fields, {
-			"server.workspace": "bags/%0_public".format([space.name]),
-			"server.page.revision": "false"
-		});
 		var adaptor = tiddler.getAdaptor();
-		adaptor.putTiddler(tiddler, null, null, callback);
+		var publish = function(original, callback) {
+			var tiddler = $.extend(new Tiddler(original.title), original);
+			tiddler.fields = $.extend({}, original.fields, {
+				"server.workspace": "bags/%0_public".format([space.name]),
+				"server.page.revision": "false"
+			});
+			adaptor.putTiddler(tiddler, null, null, callback);
+		};
+		// ensure binary tiddlers contain authentic body
+		if(!original.fields["server.content-type"]) {
+			publish(original, callback);
+		} else {
+			var _callback = function(context, userParams) {
+				publish(context.tiddler, callback);
+			};
+			var context = {
+				host: original.fields["server.host"],
+				workspace: original.fields["server.workspace"]
+			};
+			adaptor.getTiddler(original.title, context, null, _callback);
+		}
 	}
 };
 
