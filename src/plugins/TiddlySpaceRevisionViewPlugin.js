8af7ccd4a9b6b2e82fd6faa5b92cef59da8a98d4
Jon Robson <jdlrobson@gmail.com>
2010-07-29 15:29:12 +0100

    Added macros used within ViewTemplate
    The follow macro and RevisionsView macros have been left with stubs as both
    are not quite ready for production.
    
    The TiddlySpaceTiddlerIconsPlugin adds a SiteIcon view to the view macro
    to allow easy access to SiteIcons
    eg. <<view modifier SiteIcon>>
    it also makes available <<tiddlerOrigin>>
    this helps the user distinguish between private/public/external tiddlers
    This plugin makes the plugin TiddlySpaceVisualization redundant.

---
+++ src/plugins/TiddlySpaceRevisionViewPlugin.js
@@ -0,0 +1,16 @@
+/***
+|''Name''|TiddlySpaceRevisionViewPlugin|
+|''Notes''|To minimise disruption to the ViewTemplate and in anticipation of a future feature this has been written as a stub|
+!Code
+***/
+/*{{{*/
+(function($) {
+
+config.macros.viewRevisions = {
+	handler: function() {
+		
+	}
+};
+
+})(jQuery);
+/*}}}*/
