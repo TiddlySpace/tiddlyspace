ba6c8aee456f58d622c8e08d70890630de9d4dc9
FND <fnd@oda.(none)>
2010-08-03 12:35:12 +0100

    added deltiddler twanager command
    This is useful for removing obsolete tiddlers post-deployment.
    While that should eventually be handled automatically, this is a first
    step towards that goal.

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -41,7 +41,7 @@ def init(config):
 
     @make_command()
     def addmember(args):
-        """Add a member to a space. <space name> <user name>"""
+        """Add a member to a space: <space name> <user name>"""
         store = get_store(config)
         space_name, username = args
         change_space_member(store, space_name, add=username)
@@ -49,11 +49,34 @@ def init(config):
 
     @make_command()
     def delmember(args):
-        """Delete a member from a space. <space name> <user name>"""
+        """Delete a member from a space: <space name> <user name>"""
         store = get_store(config)
         space_name, username = args
         change_space_member(store, space_name, remove=username)
         return True
+
+    @make_command()
+    def deltiddler(args):
+        """Delete a tiddler from a bag: <bag> <title>"""
+        from tiddlyweb.model.tiddler import Tiddler
+        from tiddlyweb.store import NoTiddlerError
+        from tiddlyweb.util import std_error_message
+        bag, title = args
+        prompt = 'deleting tiddler %s from bag %s - enter "yes" to confirm' % (
+                title, bag)
+        if raw_input('%s\n' % prompt) == 'yes':
+            store = get_store(config)
+            tiddler = Tiddler(title, bag)
+            try:
+                store.delete(tiddler)
+            except NoTiddlerError:
+                std_error_message(
+                        'error deleting deleting tiddler %s from bag %s: %s' % (
+                            title, bag, 'no such tiddler'))
+            return True
+        else:
+            std_error_message('aborted')
+            return False
 
     merge_config(config, space_config)
 
