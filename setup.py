ac756c2aa29a5b40fe9ef74fe84888bd1caf3270
FND <fnd@fnd-oda.(none)>
2010-08-13 16:59:16 +0200

    post-deployment version increment
    (ab)using version number to indicate deployments is temporary crutch
    
    also updated TiddlyWebWiki requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Osmosoft'
 AUTHOR_EMAIL = 'tiddlyspace@osmosoft.com'
 NAME = 'tiddlywebplugins.tiddlyspace'
 DESCRIPTION = 'A discoursive social model for TiddlyWiki'
-VERSION = '0.5.1' # N.B.: duplicate of tiddlywebplugins.tiddlyspace.__init__
+VERSION = '0.5.2' # N.B.: duplicate of tiddlywebplugins.tiddlyspace.__init__
 
 
 import os
@@ -25,7 +25,7 @@ setup(
     install_requires = [
         'setuptools',
         'tiddlyweb>=1.1.dev5',
-        'tiddlywebwiki>=0.36',
+        'tiddlywebwiki>=0.39',
         'tiddlywebplugins.utils>=1.0',
         'tiddlywebplugins.logout>=0.6',
         'tiddlywebplugins.virtualhosting',
