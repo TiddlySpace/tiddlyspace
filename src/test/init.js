818d91b1b52bfbb6bae7eff6d6400771a6f14d49
FND <fnd@fnd-oda.(none)>
2010-08-20 13:35:22 +0200

    renamed mock variable in tests
    log seems more expressive/descriptive (and less misleading) than data

--- src/test/init.js
+++ src/test/init.js
@@ -3,32 +3,32 @@
 var tiddlyspace = config.extensions.tiddlyspace;
 var macro = config.macros.TiddlySpaceInit;
 
-var data = {};
+var log = {};
 var stash = {};
 
 module("init", {
 	setup: function() {
 		stash.firstRun = macro.firstRun;
 		macro.firstRun = function() {
-			data.firstRun = true;
+			log.firstRun = true;
 			stash.firstRun.apply(this, arguments);
 		};
 
 		stash.update = macro.update;
 		macro.update = function() {
-			data.update = true;
+			log.update = true;
 			stash.update.apply(this, arguments);
 		};
 
 		stash.createAvatar = macro.createAvatar;
 		macro.createAvatar = function() {
-			data.avatar = true;
+			log.avatar = true;
 		};
 
 		stash.RandomColorPalette = config.macros.RandomColorPalette;
 		config.macros.RandomColorPalette = {
 			generatePalette: function() {
-				data.palette = true;
+				log.palette = true;
 			}
 		}
 	},
@@ -38,8 +38,8 @@ module("init", {
 		macro.createAvatar = stash.createAvatar;
 		config.macros.RandomColorPalette = stash.RandomColorPalette;
 
-		$.each(data, function(key, value) {
-			delete data[key];
+		$.each(log, function(key, value) {
+			delete log[key];
 		});
 		$.each(stash, function(key, value) {
 			delete stash[key];
@@ -48,17 +48,17 @@ module("init", {
 });
 
 test("firstRun", function() {
-	strictEqual(data.firstRun, undefined);
-	strictEqual(data.update, undefined);
-	strictEqual(data.palette, undefined);
-	strictEqual(data.avatar, undefined);
+	strictEqual(log.firstRun, undefined);
+	strictEqual(log.update, undefined);
+	strictEqual(log.palette, undefined);
+	strictEqual(log.avatar, undefined);
 
 	macro.handler(null, "TiddlySpaceInit", null, null, null, null);
 
-	strictEqual(data.firstRun, true);
-	strictEqual(data.update, undefined);
-	strictEqual(data.palette, true);
-	strictEqual(data.avatar, true);
+	strictEqual(log.firstRun, true);
+	strictEqual(log.update, undefined);
+	strictEqual(log.palette, true);
+	strictEqual(log.avatar, true);
 	var flagTiddler = store.getTiddler("fooSetupFlag");
 	strictEqual(flagTiddler.fields.tiddlyspaceinit_version, "0.2");
 });
@@ -70,17 +70,17 @@ test("update from v0.1", function() {
 	};
 	store.saveTiddler(tid);
 
-	strictEqual(data.firstRun, undefined);
-	strictEqual(data.update, undefined);
-	strictEqual(data.palette, undefined);
-	strictEqual(data.avatar, undefined);
+	strictEqual(log.firstRun, undefined);
+	strictEqual(log.update, undefined);
+	strictEqual(log.palette, undefined);
+	strictEqual(log.avatar, undefined);
 
 	macro.handler(null, "TiddlySpaceInit", null, null, null, null);
 
-	strictEqual(data.firstRun, undefined);
-	strictEqual(data.update, true);
-	strictEqual(data.palette, undefined);
-	strictEqual(data.avatar, true);
+	strictEqual(log.firstRun, undefined);
+	strictEqual(log.update, true);
+	strictEqual(log.palette, undefined);
+	strictEqual(log.avatar, true);
 	var flagTiddler = store.getTiddler("fooSetupFlag");
 	strictEqual(flagTiddler.fields.tiddlyspaceinit_version, "0.2");
 });
@@ -92,17 +92,17 @@ test("update from v0.2", function() {
 	};
 	store.saveTiddler(tid);
 
-	strictEqual(data.firstRun, undefined);
-	strictEqual(data.update, undefined);
-	strictEqual(data.palette, undefined);
-	strictEqual(data.avatar, undefined);
+	strictEqual(log.firstRun, undefined);
+	strictEqual(log.update, undefined);
+	strictEqual(log.palette, undefined);
+	strictEqual(log.avatar, undefined);
 
 	macro.handler(null, "TiddlySpaceInit", null, null, null, null);
 
-	strictEqual(data.firstRun, undefined);
-	strictEqual(data.update, undefined);
-	strictEqual(data.palette, undefined);
-	strictEqual(data.avatar, undefined);
+	strictEqual(log.firstRun, undefined);
+	strictEqual(log.update, undefined);
+	strictEqual(log.palette, undefined);
+	strictEqual(log.avatar, undefined);
 });
 
 })();
