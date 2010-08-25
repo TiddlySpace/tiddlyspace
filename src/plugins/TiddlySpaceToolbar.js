6c4ac62b1276a81e54c8ebefef5f9f4695e9e754
FND <fnd@fnd-oda.(none)>
2010-08-25 10:06:27 +0200

    simplified support for icons in the tiddler toolbar's More menu
    rendered duplicate functionality obsolete by extending the selector in
    augmentCommandButtons; now augments all buttons the toolbar (rather than
    just direct descendants)
    
    This reverts commits 1caf0bc407cc4f44e61e6e274250faacc3d6a334 and
    64f6fcfe035deb6914ae22644b5732ef92869577.

--- src/plugins/TiddlySpaceToolbar.js
+++ src/plugins/TiddlySpaceToolbar.js
@@ -56,11 +56,6 @@ macro.handler = function(place, macroName, params, wikifier,
 		// otherwise the click event is the popup AND the slider
 		$(".moreCommand", place)[0].onclick = macro.onClickMorePopUp;
 	}
-	else if ( parsedParams.icons && parsedParams.icons == "yes" && 
-			parsedParams.more && parsedParams.more == "slider") {
-		// initialize the slider with icons.
-		$(".moreCommand", place)[0].onclick = macro.onClickMoreSlider;		
-	}
 	return status;
 };
 
@@ -71,7 +66,7 @@ macro.refresh = function(place, params) {
 
 var imageMacro = config.macros.image;
 macro.augmentCommandButtons = function(toolbar) {
-	$(toolbar).children(".button").each(function(i, el) {
+	$(".button", toolbar).each(function(i, el) {
 		var cmd = el.className.match(/\bcommand_([^ ]+?)\b/); // XXX: gratuitous RegEx?
 		cmd = cmd ? cmd[1] : "moreCommand"; // XXX: special-casing of moreCommand due to ticket #1234
 		var icon = store.tiddlerExists(cmd) ? cmd : macro.icons[cmd];
@@ -81,15 +76,6 @@ macro.augmentCommandButtons = function(toolbar) {
 			imageMacro.renderImage(el, icon, { alt: text });
 		}
 	});
-};
-
-// OnClickMore slider
-macro.onClickMoreSlider = function(ev) {
-    var e = this.nextSibling;
-    e.style.display = "inline";
-    this.style.display = "none";
-    macro.augmentCommandButtons(e); 
-    return false;
 };
 
 // provide onClickMore to provide extra commands in a popup
