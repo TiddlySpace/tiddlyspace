6630867e7b1acdc1212a1a1e34fe6845540a2491
FND <fnd@fnd-oda.(none)>
2010-09-01 17:35:05 +0200

    updated deployment script to erase all tiddlers from core bags
    This simplifies deployment by not requiring awareness of which tiddlers
    have been removed or renamed.

--- deploy.sh
+++ deploy.sh
@@ -26,6 +26,12 @@ if [ -n "$1" ]; then
 fi
 
 host="${log_name}${host}"
+sql="DELETE FROM revision WHERE bag_name='system' OR bag_name='tiddlyspace' \
+    OR bag_name='system-plugins_public' \
+    OR bag_name='system-info_public' \
+    OR bag_name='system-images_public' \
+    OR bag_name='system-theme_public'"
 ssh $host "sudo pip install --upgrade $pip_options $package_name && " \
+    "mysql -u tiddlyweb tiddlyspace -e \"${sql}\" && " \
     "cd $instance_dir && sudo -u $remote_sudo_id twanager update && " \
     "sudo apache2ctl restart && echo INFO: deployment complete"
