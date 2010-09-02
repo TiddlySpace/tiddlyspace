4396dc80bc78f257928f11bf094de4915ddeaab3
Jon Robson <jdlrobson@gmail.com>
2010-09-02 11:08:56 +0100

    added tests for following
    fleshed out fixtures with required functions.
    also altered Following plugin to only request adaptor when needed.

--- src/test/fixtures/tiddlywiki.js
+++ src/test/fixtures/tiddlywiki.js
@@ -41,9 +41,53 @@ Tiddler.prototype.incChangeCount = NOP;
 
 TiddlyWiki = function() {
 	this._tiddlers = {};
+	var tiddlers = this._tiddlers;
+	this.forEachTiddler = function(callback) {
+		for(var t in tiddlers) {
+			var tiddler = tiddlers[t];
+			if(tiddler instanceof Tiddler)
+				callback.call(this, t, tiddler);
+		}
+	};
 };
 TiddlyWiki.prototype.getTiddler = function(title) {
 	return this._tiddlers[title];
+};
+TiddlyWiki.prototype.getTiddlers = function(field,excludeTag)
+{
+	var results = [];
+	this.forEachTiddler(function(title, tiddler) {
+		if(excludeTag == undefined || !tiddler.isTagged(excludeTag))
+			results.push(tiddler);
+	});
+	if(field) {
+		results.sort(function(a,b) {return a[field] < b[field] ? -1 : (a[field] == b[field] ? 0 : +1);});
+	}
+	return results;
+};
+TiddlyWiki.prototype.getTaggedTiddlers = function(tag, sortField)
+{
+	return this.reverseLookup("tags", tag, true, sortField);
+};
+TiddlyWiki.prototype.reverseLookup = function(lookupField, lookupValue, 
+	lookupMatch,sortField) {
+	var results = [];
+	this.forEachTiddler(function(title, tiddler) {
+		var f = !lookupMatch;
+		for(var lookup = 0; lookup < tiddler[lookupField].length; lookup++) {
+			if(tiddler[lookupField][lookup] == lookupValue) {
+				f = lookupMatch;
+			}
+		}
+		if(f) {
+			results.push(tiddler);
+		}
+	});
+	if(!sortField) {
+		sortField = "title";
+	}
+	results.sort(function(a,b) {return a[sortField] < b[sortField] ? -1 : (a[sortField] == b[sortField] ? 0 : +1);});
+	return results;
 };
 TiddlyWiki.prototype.addTiddler = function(tiddler) {
 	this._tiddlers[tiddler.title] = tiddler;
