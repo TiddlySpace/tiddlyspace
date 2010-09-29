191181b75c26c10bccae266e24135f3106847298
Chris Dent <chris.dent@gmail.com>
2010-09-29 17:17:31 +0100

    release 0.9.23
    * new default theme based on colm-lattice2
    * optionally clickable siteicons
    * more interesting and useful 404 error pages
    * XHRs have ControlView disabled by default (where possible)
    * updates for public, private, draft tabs to ignore excludeLists
    * _hash attribute on tiddlers
    * following cleanups brought in from following space
    * core icons now pngs

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -21,7 +21,7 @@ from tiddlywebplugins.tiddlyspace.spaces import (
         add_spaces_routes, change_space_member)
 from tiddlywebplugins.prettyerror import PrettyHTTPExceptor
 
-__version__ = '0.9.22'
+__version__ = '0.9.23'
 
 
 def init(config):
