78641b1a5d027d0ce861b5cef6fc5d39e6ccbe48
Jon Robson <jdlrobson@gmail.com>
2010-10-01 11:42:58 +0100

    TiddlySpaceFollowing refactor
    * generalised the scan macro. Now much more powerful/customisable.
    * Adjusted template mechanism - now allows you to use TiddlyWiki macros on output
    eg. <<view title text>> will show the title of the tiddler in the search result sent
    from tsScan.
    * Now templates for FollowersTemplate/FollowingTemplate/ScanTemplate and can pass your own template
    * removed reply button on external tiddlers that happen to have same name as your tiddler
    instead added link to your own version - replying in this way didnt seem to work.
    * fleshed out tests.
    * to address 339, following button doesnt appear if not logged in.

--- src/test/test_TiddlySpaceFollowing.js
+++ src/test/test_TiddlySpaceFollowing.js
@@ -17,6 +17,68 @@ module("TiddlySpaceFollowing", {
 	}
 });
 
+test("getFollowerSpaceName", function() {
+	var followMacro = config.macros.followTiddlers;
+	var r1 = followMacro.getFollowerSpaceName("@jon");
+	var r2 = followMacro.getFollowerSpaceName("jon");
+	var r3 = followMacro.getFollowerSpaceName("JoN");
+	strictEqual(r1, "jon");
+	strictEqual(r2, "jon");
+	strictEqual(r3, "jon");
+});
+
+test("getOptions", function() {
+	// setup
+	var scanMacro = config.macros.tsScan;
+	var paramString = {
+		parseParams: function() {
+			return [{ tag: ["foo", "bar"], searchField:["modifier"], modifier: ["bob"], 
+				searchValues: ["jon", "fnd"], query: ["select=sort:modified"]
+			}]
+		}
+	};
+	var paramString2 = {
+		parseParams: function() {
+			return [{ tag: ["foo", "bar"], searchField:["title"], title: ["max"],
+				searchValues: ["jon", "fnd"], query: ["select=sort:modified"]
+			}]
+		}
+	};
+	var paramString3 = {
+		parseParams: function() {
+			return [{ tag: ["foo", "bar"], searchField:["modifier"],
+				searchValues: ["jon", "fnd"], query: ["select=sort:modified"]
+			}]
+		}
+	};
+	var paramString4 = {
+		parseParams: function() {
+			return [{ tag: ["foo", "bar"], searchField:["modifier"],
+				name: ["SiteInfo"], query: ["select=sort:modified"]
+			}]
+		}
+	};
+
+	// run
+	var options = scanMacro.getOptions(paramString, "http://%0.tiddlyspace.com");
+	var options2 = scanMacro.getOptions(paramString2);
+	var options3 = scanMacro.getOptions(paramString3);
+	var options4 = scanMacro.getOptions(paramString4);
+
+	// test
+	strictEqual(options.spaceField, "bag");
+	strictEqual(options.searchField, "modifier");
+	strictEqual(options.tag, "foo");
+	strictEqual(options.searchValues[0], "bob");
+	strictEqual(options.fat, false);
+	strictEqual(options.query, "select=sort:modified");
+	strictEqual(options.host, "http://%0.tiddlyspace.com");
+	strictEqual(options2.searchValues[0], "max");
+	strictEqual(options3.searchValues.length, 2);
+	strictEqual(options3.searchValues.indexOf("jon") > -1, true);
+	strictEqual(options3.searchValues.indexOf("fnd") > -1, true);
+	strictEqual(options4.searchValues[0], "SiteInfo");
+});
 
 test("_constructBagQuery", function() {
 	var followMacro = config.macros.followTiddlers;
