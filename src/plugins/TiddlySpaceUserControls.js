b027e3268624ef02f131055547bdc99f51441e7f
Jon Robson <jdlrobson@gmail.com>
2010-12-03 15:27:36 +0000

    login uses ajaxReq rather than jQuery ajax
    This is significant as without this you cannot login using
    Firefox off a file uri.

--- src/plugins/TiddlySpaceUserControls.js
+++ src/plugins/TiddlySpaceUserControls.js
@@ -1,6 +1,6 @@
 /***
 |''Name''|TiddlySpaceUserControls|
-|''Version''|0.5.4|
+|''Version''|0.5.5|
 |''Description''|registration and login UIs|
 |''Status''|@@beta@@|
 |''Source''|http://github.com/TiddlySpace/tiddlyspace/raw/master/src/plugins/TiddlySpaceUserControls.js|
@@ -137,7 +137,7 @@ var tsl = config.macros.TiddlySpaceLogin = {
 	login: function(username, password, callback, errback) {
 		var challenger = "cookie_form";
 		var uri = "%0/challenge/%1".format([tweb.host, challenger]);
-		$.ajax({
+		ajaxReq({
 			url: uri,
 			type: "POST",
 			data: {
