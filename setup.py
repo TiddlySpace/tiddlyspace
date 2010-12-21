0142ca833f8fd9ef1d10bbae37867bea032ff171
Chris Dent <chris.dent@gmail.com>
2010-12-21 15:42:40 +0000

    release 0.9.52
    * import wiki files and urls to private by default, but give choice
    * add an optional lable to the reply to this tiddler macro
    * fix safe mode to correctly use the csrf token

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Osmosoft'
 AUTHOR_EMAIL = 'tiddlyspace@osmosoft.com'
 NAME = 'tiddlywebplugins.tiddlyspace'
 DESCRIPTION = 'A discoursive social model for TiddlyWiki'
-VERSION = '0.9.51' # NB: duplicate of tiddlywebplugins.tiddlyspace.__init__
+VERSION = '0.9.52' # NB: duplicate of tiddlywebplugins.tiddlyspace.__init__
 
 
 import os
--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -31,7 +31,7 @@ from tiddlywebplugins.prettyerror import PrettyHTTPExceptor
 import tiddlywebplugins.status
 
 
-__version__ = '0.9.51'
+__version__ = '0.9.52'
 
 
 def init(config):
