c01e7b6c43b16032e843275332d77a5991561818
Jon Robson <jdlrobson@gmail.com>
2011-01-19 16:40:46 +0000

    Fixed bug in spaceLink view type
    If the external parameter is used and a tiddler belongs to a space
    an external link is not created thus tiddler and tiddlyspace attributes
    are not set.
    
    This was resulting in an attempt to open an undefined tiddler in bag
    undefined. This wraps this special case addressing 578

--- src/plugins/TiddlySpaceFollowingPlugin.js
+++ src/plugins/TiddlySpaceFollowingPlugin.js
@@ -1,6 +1,6 @@
 /***
 |''Name''|TiddlySpaceFollowingPlugin|
-|''Version''|0.6.15|
+|''Version''|0.6.16|
 |''Description''|Provides a following macro|
 |''Author''|Jon Robson|
 |''Requires''|TiddlySpaceConfig TiddlySpaceTiddlerIconsPlugin|
@@ -457,10 +457,14 @@ config.macros.view.views.spaceLink = function(value, place, params, wikifier,
 		var link = createSpaceLink(place, spaceName, title, label);
 		if(args.external && args.external[0] == "no") {
 			$(link).click(function(ev) {
-				ev.preventDefault();
 				var el = $(ev.target);
-				tiddlyspace.displayServerTiddler(el[0], el.attr("tiddler"),
-					"bags/%0_public".format( el.attr("tiddlyspace") ));
+				var title = el.attr("tiddler");
+				var space = el.attr("tiddlyspace");
+				if(title && space) {
+					ev.preventDefault();
+					tiddlyspace.displayServerTiddler(el[0], title,
+						"bags/%0_public".format( space ));
+				}
 				return false;
 			});
 		}
