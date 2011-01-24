fb2fd323bb8871d307daa7666445bdcf74cd13a9
Chris Dent <chris.dent@gmail.com>
2011-01-24 08:34:08 +0000

    Delete from both revision and tiddler, otherwise we get some very strange results and errors.
    (The tiddler table points to non-existent revisions.)
    
    Ideally our database would support cascading delete, but it does not.
    Ideally we would also handle the field, tag and text table in these
    calls.

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,12 @@ sql="DELETE FROM revision WHERE bag_name='system' OR bag_name='tiddlyspace' \
     OR bag_name='system-plugins_public' \
     OR bag_name='system-info_public' \
     OR bag_name='system-images_public' \
-    OR bag_name='system-theme_public'"
+    OR bag_name='system-theme_public'; \
+    DELETE FROM tiddler WHERE bag_name='system' OR bag_name='tiddlyspace' \
+    OR bag_name='system-plugins_public' \
+    OR bag_name='system-info_public' \
+    OR bag_name='system-images_public' \
+    OR bag_name='system-theme_public';"
 ssh $host "sudo pip install --upgrade $pip_options $package_name && " \
     "mysql -u tiddlyweb tiddlyspace -e \"${sql}\" && " \
     "cd $instance_dir && sudo -u $remote_sudo_id twanager update && " \
