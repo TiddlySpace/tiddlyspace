9c51bfa90a30bc83ae16f70888f06c467de07ceb
Jon Robson <jdlrobson@gmail.com>
2011-02-08 13:15:45 +0000

    disable error handler for SystemSettings tiddler
    this can be confusing for users and shouldn't be opened.

--- src/plugins/ErrorHandler.js
+++ src/plugins/ErrorHandler.js
@@ -1,6 +1,6 @@
 /***
 |''Name''|ErrorHandlerPlugin|
-|''Version''|0.4.2|
+|''Version''|0.4.3|
 |''Author''|Jon Robson|
 |''Description''|Localised tiddler save errors including edit conflict resolution.|
 |''CoreVersion''|2.6.1|
@@ -52,6 +52,9 @@ var msgs = config.messages.editConflict = {
 var plugin = config.extensions.errorHandler = {
 	diffTags: ["excludeLists", "excludeMissing", "excludeSearch"],
 	displayMessage: function(message, tiddler, context) {
+		if(title === "SystemSettings") {
+			return;
+		}
 		var desc = context && context.httpStatus ? context.statusText :
 			sssp.locale.connectionError;
 		var reportArea = plugin.reportError(tiddler.title);
