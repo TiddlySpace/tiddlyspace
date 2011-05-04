f1cbdd6e0973e72a9733881a060de55f74f6abcc
Chris Dent <chris.dent@gmail.com>
2011-05-04 15:29:39 +0100

    release 1.0.2
    * include the templates

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 
 def init(config):
