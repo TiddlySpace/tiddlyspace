d1db10894219fb4b05101fe586d120c41efcf645
Chris Dent <chris.dent@gmail.com>
2011-05-05 17:27:07 +0100

    release 1.0.5
    * fix bug in associating open id
    * fix error in search results template

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 
 def init(config):
