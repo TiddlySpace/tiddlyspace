f04da49239e3d7d11db916c218b0fa043ada640f
Chris Dent <chris.dent@gmail.com>
2011-05-23 15:11:43 +0100

    release 1.0.13
    * Include tiddlywiki alpha 2.6.3 A7, which has jquery 1.6.1

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.12'
+__version__ = '1.0.13'
 
 
 def init(config):
