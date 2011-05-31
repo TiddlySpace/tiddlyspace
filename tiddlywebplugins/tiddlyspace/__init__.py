ecbed5fc9d03a675a827d54033e004403a4e9ce9
Chris Dent <chris.dent@gmail.com>
2011-05-31 15:37:36 +0100

    release 1.0.14
    * encoding and escaping improvements in html representation templates
    * add hcard and valid html to profile template
    * bring TiddlySpaceLinkPlugin into local repo

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.13'
+__version__ = '1.0.14'
 
 
 def init(config):
