1fc664442d3d3d7dc47e7409cbce4c9950c3c5e0
Chris Dent <chris.dent@gmail.com>
2011-06-13 17:38:40 +0100

    Display the original error message when giving a 404.
    Otherwise we have no way of knowing what went wrong.

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.16'
+__version__ = '1.0.17'
 
 
 def init(config):
