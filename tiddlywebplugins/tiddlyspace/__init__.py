d3c271840d304c9c455a5b94db90de4201317ba2
Chris Dent <chris.dent@gmail.com>
2011-06-28 13:58:01 +0100

    release 1.0.20
    * Bug fixes for import in IE 6/7
    * Improvements to HTML serialization customizing
    * Cleanups in presentation of login and registation

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.19'
+__version__ = '1.0.20'
 
 
 def init(config):
