354dd3014489b75b5f58c4c99eab613647fc8860
Chris Dent <chris.dent@gmail.com>
2011-07-13 17:37:42 +0100

    release 1.0.25
    * correct error in html sanitization when including tiddlers from
      other spaces

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.24'
+__version__ = '1.0.25'
 
 
 def init(config):
