f8d797e99dff46642d6dc4663eb8e42c01e210e5
Chris Dent <chris.dent@gmail.com>
2011-07-19 15:37:23 +0100

    release 1.0.29
    * limit manipulating members to a space requests to that space

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.28'
+__version__ = '1.0.29'
 
 
 def init(config):
