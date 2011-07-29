75a4c71dd3e5a5433ca9e80e54da49109740676f
Chris Dent <chris.dent@gmail.com>
2011-07-29 13:03:15 +0100

    release 1.0.31
    * better HTML serialization

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.30'
+__version__ = '1.0.31'
 
 
 def init(config):
