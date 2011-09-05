047d149d78d91d2cdf55d66796cfe3966c7d14d0
Chris Dent <chris.dent@gmail.com>
2011-09-05 16:48:04 +0100

    release 1.0.36
    * fix typo in chrjs.space.includes

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -22,7 +22,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.35'
+__version__ = '1.0.36'
 
 
 def init(config):
