9b4d7cb9069316fc5ec208f1358e0886719735d5
Jon Robson <jdlrobson@gmail.com>
2011-09-07 16:15:20 +0100

    add apps bag with app switcher code
    The idea is an app on tiddlyspace can include
    <script type="text/javascript" src="/bags/apps/tiddlers/ts-application.js"></script>
    
    and it will provide a way of switching between apps on TiddlySpace
    
    it loads a file 'app-picker.html' into an iframe which is yet to be defined
    
    it is important this content lives in a separate bag that is not included
    in any recipe so that it doesn't clutter up existing users TiddlyWiki's

---
+++ src/apps/ts-application.js
@@ -0,0 +1,63 @@
+/***
+Adds the app switcher to a TiddlySpace app.
+***/
+(function() {
+
+window.addEventListener("load", function() {
+	var link = document.createElement("a");
+	link.setAttribute("id", "app-picker");
+	link.setAttribute("class", "app-picker");
+	link.appendChild(document.createTextNode("tiddlyspace"));
+
+	var body = document.getElementsByTagName("BODY")[0];
+	body.appendChild(link);
+	var html = [
+	'<div class="bubble">',
+	    '<iframe src="/bags/apps/tiddlers/app-picker" width="auto" style="border:none;"></iframe>',
+	    '<div class="tail"></div>',
+	'</div>'].join("");
+	var bubble = document.createElement("div");
+	bubble.setAttribute("id", "bs-popup");
+	bubble.setAttribute("style", "display:none;");
+	bubble.setAttribute("class", "bs-popup");
+	bubble.innerHTML = html;
+	body.appendChild(bubble);
+
+	var head = document.getElementsByTagName("HEAD")[0];
+	var el = document.createElement("link");
+	el.setAttribute("rel", "stylesheet");
+	el.setAttribute("href", "/bags/apps/tiddlers/ts-application.css");
+	el.setAttribute("type", "text/css");
+	head.appendChild(el);
+
+	var bubbleFadeInterval;
+	function fade(el, fadeIn) {
+		var opacity = fadeIn ? 0 : 1;
+		if(bubbleFadeInterval) {
+			clearInterval(bubbleFadeInterval);
+		}
+		bubbleFadeInterval = setInterval(function() {
+			el.style.cssText = "opacity:" + opacity;
+			opacity = fadeIn ? opacity + 0.1 : opacity - 0.1;
+			if(opacity < 0 || opacity > 1) {
+				clearInterval(bubbleFadeInterval);
+				el.style.cssText = fadeIn ? "" : "display:none";
+			}
+		}, 50);
+	}
+
+	link.addEventListener("mousedown", function(ev) {
+		ev.preventDefault();
+	});
+
+	var bubbleOpen = false;
+	link.addEventListener("click", function(ev) {
+		if(bubbleOpen) {
+			fade(bubble, false);
+		} else {
+			fade(bubble, true);
+		}
+		bubbleOpen = !bubbleOpen;
+	});
+});
+})();
