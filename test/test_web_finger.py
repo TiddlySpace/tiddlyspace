9b9002d32a40f8d8385723a811a91c0b9d1351c0
Ben Gillies <bengillies@gmail.com>
2011-09-09 16:17:25 +0100

    Fixed the webfinger hcard test
    I modified the HTML, so the match wasn't working. The resultant hcard is
    now reflected in the test, which now passes

--- test/test_web_finger.py
+++ test/test_web_finger.py
@@ -83,8 +83,8 @@ def test_vcard():
     response, content = http.request('http://0.0.0.0:8080/profiles/cdent')
     assert response['status'] == '200'
 
-    assert '<div id="hcard-cdent" class="vcard"' in content
+    assert '<h1 id="hcard-cdent" class="vcard"' in content
     assert 'src="/recipes/cdent_public/tiddlers/SiteIcon" alt="avatar" class="photo"' in content
-    assert '<div class="fn"><a href="http://cdent.0.0.0.0:8080/" class="url">cdent</a></div>' in content
+    assert '<a href="http://cdent.0.0.0.0:8080/" class="fn url">cdent</a>' in content
 
 
