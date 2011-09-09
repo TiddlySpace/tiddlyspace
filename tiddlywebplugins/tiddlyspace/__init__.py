1aebed4d6768f95f07cc13de0c81b2d71ad311bd
Chris Dent <chris.dent@gmail.com>
2011-09-09 20:09:27 +0100

    Make tsprofile.html work properly with tiddler_url
    In order for it to work we need to make sure it gets overidden
    before other imports, so the overrides have been moved to fixups
    which get imported early in __init__.py

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -6,12 +6,8 @@ website: http://tiddlyspace.com
 repository: http://github.com/TiddlySpace/tiddlyspace
 """
 
+import tiddlywebplugins.tiddlyspace.fixups
 
-import tiddlywebplugins.status
-import tiddlyweb.web.util
-
-from tiddlyweb.model.user import User
-from tiddlyweb.store import NoUserError
 from tiddlyweb.util import merge_config
 from tiddlyweb.web.http import HTTPExceptor
 
@@ -20,7 +16,6 @@ from tiddlywebplugins.instancer.util import get_tiddler_locations
 from tiddlywebplugins.tiddlyspace.commands import establish_commands
 from tiddlywebplugins.tiddlyspace.config import config as space_config
 from tiddlywebplugins.tiddlyspace.instance import store_contents
-from tiddlywebplugins.tiddlyspace.space import Space
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
@@ -102,70 +97,3 @@ def init(config):
             'text/html; charset=UTF-8']
     config['serializers']['text/html'] = new_serializer
     config['serializers']['default'] = new_serializer
-
-
-original_gather_data = tiddlywebplugins.status._gather_data
-
-
-def _status_gather_data(environ):
-    """
-    Monkey patch twp.status to add additional information
-    specific to TiddlySpace.
-    """
-    data = original_gather_data(environ)
-    data['server_host'] = environ['tiddlyweb.config']['server_host']
-    data['tiddlyspace_version'] = __version__
-    # ensure user is known
-    usersign = environ['tiddlyweb.usersign']['name']
-    store = environ['tiddlyweb.store']
-    try:
-        store.get(User(usersign))
-    except NoUserError:
-        data['username'] = 'GUEST'
-        if usersign != 'GUEST':
-            data['identity'] = usersign
-    return data
-
-
-tiddlywebplugins.status._gather_data = _status_gather_data
-
-
-original_tiddler_url = tiddlyweb.web.util.tiddler_url
-
-
-def web_tiddler_url(environ, tiddler, container='bags', full=True):
-    """
-    Override default tiddler_url to be space+host aware.
-
-    If the bag or recipe of the tiddler is of a space, switch to
-    that space's host for the duration of uri creation.
-
-    Do this all the time, so that we get the right URIs even
-    when working around ControlView.
-    """
-    saved_host = environ.get('HTTP_HOST', '')
-    try:
-        if container == 'recipes':
-            space_name = Space.name_from_recipe(tiddler.recipe)
-        else:
-            space_name = Space.name_from_bag(tiddler.bag)
-
-        host = environ['tiddlyweb.config']['server_host']['host']
-        port = environ['tiddlyweb.config']['server_host']['port']
-        if port is '443' or port is '80':
-            port = ''
-        else:
-            port = ':%s' % port
-        environ['HTTP_HOST'] = '%s.%s%s' % (space_name.encode('utf-8'),
-            host, port)
-    except ValueError:
-        pass
-    url = original_tiddler_url(environ, tiddler, container, full)
-    if saved_host:
-        environ['HTTP_HOST'] = saved_host
-    elif 'HTTP_HOST' in environ:
-        del environ['HTTP_HOST']
-    return url
-
-
-tiddlyweb.web.util.tiddler_url = web_tiddler_url
---
+++ tiddlywebplugins/tiddlyspace/fixups.py
@@ -0,0 +1,76 @@
+"""
+Override behaviors from other modules.
+"""
+
+import tiddlywebplugins.status
+import tiddlyweb.web.util
+
+from tiddlyweb.model.user import User
+from tiddlyweb.store import NoUserError
+from tiddlywebplugins.tiddlyspace.space import Space
+
+original_gather_data = tiddlywebplugins.status._gather_data
+
+
+def _status_gather_data(environ):
+    """
+    Monkey patch twp.status to add additional information
+    specific to TiddlySpace.
+    """
+    data = original_gather_data(environ)
+    data['server_host'] = environ['tiddlyweb.config']['server_host']
+    data['tiddlyspace_version'] = __version__
+    # ensure user is known
+    usersign = environ['tiddlyweb.usersign']['name']
+    store = environ['tiddlyweb.store']
+    try:
+        store.get(User(usersign))
+    except NoUserError:
+        data['username'] = 'GUEST'
+        if usersign != 'GUEST':
+            data['identity'] = usersign
+    return data
+
+
+tiddlywebplugins.status._gather_data = _status_gather_data
+
+
+original_tiddler_url = tiddlyweb.web.util.tiddler_url
+
+
+def web_tiddler_url(environ, tiddler, container='bags', full=True):
+    """
+    Override default tiddler_url to be space+host aware.
+
+    If the bag or recipe of the tiddler is of a space, switch to
+    that space's host for the duration of uri creation.
+
+    Do this all the time, so that we get the right URIs even
+    when working around ControlView.
+    """
+    saved_host = environ.get('HTTP_HOST', '')
+    try:
+        if container == 'recipes':
+            space_name = Space.name_from_recipe(tiddler.recipe)
+        else:
+            space_name = Space.name_from_bag(tiddler.bag)
+
+        host = environ['tiddlyweb.config']['server_host']['host']
+        port = environ['tiddlyweb.config']['server_host']['port']
+        if port is '443' or port is '80':
+            port = ''
+        else:
+            port = ':%s' % port
+        environ['HTTP_HOST'] = '%s.%s%s' % (space_name.encode('utf-8'),
+            host, port)
+    except ValueError:
+        pass
+    url = original_tiddler_url(environ, tiddler, container, full)
+    if saved_host:
+        environ['HTTP_HOST'] = saved_host
+    elif 'HTTP_HOST' in environ:
+        del environ['HTTP_HOST']
+    return url
+
+
+tiddlyweb.web.util.tiddler_url = web_tiddler_url
