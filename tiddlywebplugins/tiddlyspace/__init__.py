ed3997d28f6b9723761e3fdeda90ad21bbc21872
Chris Dent <chris.dent@gmail.com>
2011-10-11 17:32:04 +0100

    release 1.0.53
    * redirect to ServerSettings index: things
    * universal backstage for ie
    * better logout handling in universal backstage

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -23,7 +23,7 @@ from tiddlywebplugins.tiddlyspace.instance import store_contents
 from tiddlywebplugins.tiddlyspace.www import establish_www
 
 
-__version__ = '1.0.52'
+__version__ = '1.0.53'
 
 
 def init(config):
