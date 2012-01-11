eb87a69576d296ba6e2b5d9f61d539f729b91671
Chris Dent <chris.dent@gmail.com>
2012-01-11 17:18:32 +0000

    Only do web-based fixups when running the server.
    Otherwise twanager gets code that wants a wsgi environ, which is bad.

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -6,7 +6,6 @@ website: http://tiddlyspace.com
 repository: http://github.com/TiddlySpace/tiddlyspace
 """
 
-import tiddlywebplugins.tiddlyspace.fixups
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
@@ -18,4 +17,6 @@ def init(config):
     Establish required plugins and HTTP routes.
     """
     config['tiddlyspace.version'] = __version__
+    if 'selector' in config:
+        import tiddlywebplugins.tiddlyspace.fixups
     init_plugin(config)
