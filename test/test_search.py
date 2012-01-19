3165c0e91ea9ce6cca77980649b7011ad3780fef
Chris Dent <chris.dent@gmail.com>
2012-01-19 12:55:06 +0000

    update search tests because of addition of more tiddlers to common bag

--- test/test_search.py
+++ test/test_search.py
@@ -118,7 +118,7 @@ def test_search_no_args():
     response, content = http.request('http://0.0.0.0:8080/search.json?q=_limit:999')
     assert response['status'] == '200'
     allinfo = simplejson.loads(content)
-    assert len(allinfo) == 153
+    assert len(allinfo) == 154
 
     response, content = http.request('http://fnd.0.0.0.0:8080/search.json')
     assert response['status'] == '200'
@@ -128,6 +128,6 @@ def test_search_no_args():
     response, content = http.request('http://fnd.0.0.0.0:8080/search.json?q=_limit:999')
     assert response['status'] == '200'
     fndinfo = simplejson.loads(content)
-    assert len(fndinfo) == 134
+    assert len(fndinfo) == 135
 
     assert len(allinfo) > len(fndinfo)
