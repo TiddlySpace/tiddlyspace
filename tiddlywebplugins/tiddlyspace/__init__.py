24d03b20418f67e711daa55ab23961468749de51
Chris Dent <chris.dent@gmail.com>
2012-07-10 10:53:30 +0100

    release 1.1.7
    * give a "loading" indicator when clicking reply button

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.1.6'
+__version__ = '1.1.7'
 
 
 def init(config):
