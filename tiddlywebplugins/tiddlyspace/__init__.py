7337f1f37729cfe588bad8657fe6a0fa9cd8c4ef
Chris Dent <chris.dent@gmail.com>
2012-07-19 17:43:17 +0100

    release 1.1.13
    * friendly tag on tiddler_url
    * grammar fix on /_space tidlers info

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.1.12'
+__version__ = '1.1.13'
 
 
 def init(config):
