e66818dd9519826b50c574c823fe2bac6dc98d95
Chris Dent <chris.dent@gmail.com>
2012-08-08 15:50:12 +0100

    release 1.1.17
    * Improved presentation of HTML search results.
    * Automated Javascript tests.

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.1.16'
+__version__ = '1.1.17'
 
 
 def init(config):
