f5be65ad0e5f75fc13ab55e26963f72a74ea735b
Chris Dent <chris.dent@gmail.com>
2012-08-16 19:31:24 +0100

    release 1.1.22
    * More ts.js cleanups, including better notification on space
      inclusion

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ AUTHOR = 'Osmosoft'
 AUTHOR_EMAIL = 'tiddlyspace@osmosoft.com'
 NAME = 'tiddlywebplugins.tiddlyspace'
 DESCRIPTION = 'A discoursive social model for Tiddlers'
-VERSION = '1.1.21' # NB: duplicate of tiddlywebplugins.tiddlyspace.__init__
+VERSION = '1.1.22' # NB: duplicate of tiddlywebplugins.tiddlyspace.__init__
 
 
 import os
