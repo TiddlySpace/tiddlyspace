bf87db8a0453793bc56bc55b77ec7d61ce115f76
Chris Dent <chris.dent@gmail.com>
2012-12-15 00:11:54 +0000

    update setup.py accordingly

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     scripts = ['tiddlyspace'],
     install_requires = [
         'setuptools',
-        'tiddlyweb>=1.2.51',
+        'tiddlyweb>=1.3.0',
         'tiddlywebwiki>=0.57.0',
         'tiddlywebplugins.status>=0.6',
         'tiddlywebplugins.utils>=1.0',
@@ -51,6 +51,7 @@ setup(
         'tiddlywebplugins.csrf',
         'tiddlywebplugins.whoosher',
         'tiddlywebplugins.markdown',
+        'httpexceptor',
         'selector<0.9.0'
     ],
     include_package_data = True,
