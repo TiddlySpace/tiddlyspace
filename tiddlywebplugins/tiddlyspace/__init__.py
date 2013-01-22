bbb6907c7154d2d8690c63400caf07d2079e39a3
Chris Dent <chris.dent@gmail.com>
2013-01-22 15:41:18 +0000

    release 1.1.36
    * improved look and behavior in UniversalBackstage

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.1.35'
+__version__ = '1.1.36'
 
 
 def init(config):
