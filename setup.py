441f576b69cc689c2fa174a80938ed9909a2ed2d
Chris Dent <chris.dent@gmail.com>
2013-01-29 14:55:26 +0000

    update tiddlyweb requirement due to change in logging

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     scripts = ['tiddlyspace'],
     install_requires = [
         'setuptools',
-        'tiddlyweb>=1.3.0',
+        'tiddlyweb>=1.4.2',
         'tiddlywebwiki>=0.57.0',
         'tiddlywebplugins.status>=0.6',
         'tiddlywebplugins.utils>=1.0',
