e60295e45289badb351179ff800bc3a020271ca9
Chris Dent <chris.dent@gmail.com>
2013-02-05 13:29:10 +0000

    Release 1.1.39
    * improved ui in _space and _account
    * fixes in backstage.js
    * refined text in GettingStarted

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.1.38'
+__version__ = '1.1.39'
 
 
 def init(config):
