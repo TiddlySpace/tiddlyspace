f9f1e0e928cb42663083d52fe2f2c58c2287e3e4
Chris Dent <chris.dent@gmail.com>
2013-04-07 18:19:22 +0100

    release 1.2.2
    * fix from @pmario for ToggleTiddlerPrivacyPlugin in Firefox20
    * Warn about inclusion in the import backstage.

--- tiddlywebplugins/tiddlyspace/__init__.py
+++ tiddlywebplugins/tiddlyspace/__init__.py
@@ -9,7 +9,7 @@ repository: http://github.com/TiddlySpace/tiddlyspace
 from tiddlywebplugins.tiddlyspace.plugin import init_plugin
 
 
-__version__ = '1.2.1'
+__version__ = '1.2.2'
 
 
 def init(config):
