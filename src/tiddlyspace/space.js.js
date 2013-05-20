cce1077734c5a7f156fb0dcb1803d091504c4585
Colm Britton <colmjude@gmail.com>
2013-05-20 20:07:18 +0100

    Add a 'reset space' button that resets a space to its original state

--- src/tiddlyspace/space.js.js
+++ src/tiddlyspace/space.js.js
@@ -192,6 +192,153 @@ $(".toggleNext").each(function(i, el) {
 	toggleNext({ target: el });
 });
 
+(function($) {
+	var status = tiddlyweb.status,
+		spacename = status.space.name,
+		pub_recipe_arr = [
+			["system", ""],
+			["tiddlyspace", ""],
+			["system-plugins_public", ""],
+			["system-info_public", ""],
+			["system-images_public", ""],
+			["system-theme_public", ""],
+			[spacename + "_public", ""]
+		],
+		pri_recipe_arr = pub_recipe_arr.concat([[spacename + "_private", ""]]);
+
+	function resetRecipes() {
+		var pub_recipe = new tiddlyweb.Recipe(spacename + "_public", "/"),
+			pri_recipe = new tiddlyweb.Recipe(spacename + "_private", "/");
+
+		function callback() {}
+		function errorback() {}
+		pub_recipe.get(function(recipe, status, xhr) {
+			recipe.recipe = pub_recipe_arr;
+			recipe.put(callback, errorback);
+		}, function(xhr, error, exc) {
+		});
+		pri_recipe.get(function(recipe, status, xhr) {
+			recipe.recipe = pri_recipe_arr;
+			recipe.put(callback, errorback);
+		}, function(xhr, error, exc) {
+		});
+	}
+
+	function deleteSpaceTiddlers(cb) {
+		var host = tiddlyweb.status.server_host.host,
+			pub_url = "/bags/" + spacename + "_public/tiddlers",
+			pri_url = "/bags/" + spacename + "_private/tiddlers";
+
+		function emptyBag(url, cb) {
+			$.getJSON(url + ".json", function(data) {
+				if(data && data.length > 0) {
+					data.forEach(function(el, index, arr) {
+						$.ajax({
+							type: "DELETE",
+							url: url + "/" + el.title
+						}).done(function() {
+							if(arr.length === 1 && cb) {
+								cb();
+							}
+						});
+					});
+				} else if(cb) {
+					cb();
+				}
+			});
+		}
+
+		emptyBag(pub_url);
+		emptyBag(pri_url, cb);
+	}
+
+	function putTiddler(tiddler, url, cb) {
+		$.ajax({
+			url: url,
+			type: "PUT",
+			contentType: 'application/json',
+			data: JSON.stringify(tiddler)
+		}).done(function() {
+			if(cb) {
+				cb();
+			}
+		});
+	}
+
+	function cloneGettingStarted() {
+		var url = "bags/system-info_public/tiddlers/GettingStarted.json?fat=1",
+			gsurl = "/bags/" + spacename + "_public/tiddlers/GettingStarted";
+
+		$.getJSON(url, function(tiddler) {
+			putTiddler(tiddler, gsurl);
+		});
+	}
+
+	function createSiteInfo(cb) {
+		var siurl = "/bags/" + spacename + "_public/tiddlers/SiteInfo",
+			siTiddler = {
+			title: "SiteInfo",
+			text: "Space " + spacename,
+			tags: []
+		};
+		putTiddler(siTiddler, siurl, cb);
+	}
+
+	function resetSpace(el) {
+		resetRecipes();
+		deleteSpaceTiddlers(function() {
+			cloneGettingStarted();
+			createSiteInfo(function() {
+				$(el).addClass('resetcomplete');
+				$.event.trigger('resetcomplete');
+			});
+		});
+	};
+
+	$(function() {
+		var $resetwrap = $(".reset-confirm-wrap").hide(),
+			$resetform = $resetwrap.find("form"),
+			$msgArea = $resetwrap.find(".reset-message-area");
+
+		// need to check if user is a member
+		if(status.space.recipe.match(/_private$/)) {
+			$(".spacereset").on("click", function(e) {
+				$resetwrap
+					.show()
+					.find("input")
+						.focus();
+				return false;
+			});
+
+			$resetwrap.find(".close-btn").on("click", function() {
+				$resetform.find("input").val("");
+				$resetwrap.removeClass("error").hide();
+			});
+
+			$resetform.on("submit", function(e) {
+				var inputname = $(e.target).find("input").val();
+
+				$resetwrap.removeClass("error");
+				if(inputname === spacename) {
+					$resetwrap.addClass("inaction");
+					$msgArea.find(".performing").show();
+					$( document ).on("resetcomplete", function() {
+						$resetwrap.removeClass("inaction").addClass("resetcomplete");
+						$msgArea
+							.find(".performing").hide()
+							.end()
+							.find(".finished").show();
+					});
+					resetSpace( $(".spacereset") );
+				} else {
+					$resetwrap.addClass("error");
+				}
+				return false;
+			});
+		}
+	});
+}(jQuery));
+
 if(window !== window.top) {
 	$("html").addClass("iframeMode");
 	$("a").live("click", function(ev) {
